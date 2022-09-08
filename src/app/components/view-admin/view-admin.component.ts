8d04443f4128f1af428e543b5c379ddba7fa130d
GabrielaC3 <Gabycr.3.2.gcr@gmail.com>
2022-09-07 19:41:11 -0500

    agregar y eliminar usuarios de firebase

---
+++ src/app/components/view-admin/view-admin.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../servicios/user.service';
+import Users from '../../data/data.users';
+
+@Component({
+  selector: 'app-view-admin',
+  templateUrl: './view-admin.component.html',
+  styleUrls: ['./view-admin.component.css']
+})
+export class ViewAdminComponent implements OnInit {
+
+  users: Users[];
+  numOrden: number=0;
+
+  registerSwitch: boolean = false;
+
+  constructor(private registerM:UserService,
+    private userService: UserService,) {
+      this.users = [{
+        uid: 'xxxxxx',
+        nombre: 'remy',
+        apellido: 'ratita',
+        email: 'rratita@burger.com',
+        funcion: 'cocinero'
+      }]
+     }
+
+  ngOnInit(): void {
+    this.registerM.$register.subscribe((valor)=>{this.registerSwitch=valor})
+    this.userService.getUser().subscribe((users) => {
+      this.users = users;       
+    })
+    
+  }
+
+  openRegister(){
+    this.registerSwitch = true;
+  }
+
+  async onClickDelete( users: Users) {
+    await this.userService.deleteRegistro(users);
+    console.log(users);
+    /*this.userService.deleteUsuario(users.uid).then(response =>); */
+  }
+
+}
--- src/app/servicios/user.service.ts
+++ src/app/servicios/user.service.ts
@@ -1,12 +1,16 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth, deleteUser} from '@angular/fire/auth';
+import { Firestore, collection, addDoc, collectionData, doc, deleteDoc} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import Users from '../data/data.users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private auth: Auth) { }
+  constructor(private auth: Auth,
+    private firestore: Firestore) { }
 
   register ({ email, password }: any){
     return createUserWithEmailAndPassword(this.auth, email, password)
@@ -17,5 +21,24 @@ export class UserService {
   }
 
   $register = new EventEmitter<any>();
+
+  addUser(user: Users){
+    const userRef = collection(this.firestore, 'users');
+    return addDoc(userRef, user);
+  }
+
+  getUser(): Observable<Users[]>{
+    const userRef = collection(this.firestore, 'users');
+    return collectionData(userRef, { idField: 'id'}) as Observable<Users[]>
+  }
+
+  deleteRegistro(user: Users) {
+    const userRef  = doc(this.firestore, `users/${user.id}`);
+    return deleteDoc(userRef );
+  }
+
+/*   deleteUsuario(user: Users){
+    return deleteUser(user.uid);
+  } */
 
 }
